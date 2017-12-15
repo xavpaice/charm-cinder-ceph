194b9274a63eee61b7d2aa23b632d2b294cd71eb
Liam Young <liam.young@canonical.com>
2017-12-15 07:11:53 +0000

    Request class-read object_prefix rbd_children perm
    When using ceph as a backend request the additional privilege
    class-read on rbd_children. This fixes bug 1696073.
    
    Change-Id: I023781e01c1e314cb2755e7867cdf588432791fc
    Closes-Bug: #1696073
    Depends-On: Icf844ec7d33f2e558dee7935fe5fa3d7f08e0d59

--- unit_tests/test_cinder_hooks.py
+++ unit_tests/test_cinder_hooks.py
@@ -134,9 +134,18 @@ class TestCinderHooks(CharmTestCase):
         mock_create_pool.assert_called_with(name='cinder', replica_count=4,
                                             weight=20, group='volumes')
         mock_request_access.assert_has_calls([
-            call(name='volumes', permission='rwx'),
-            call(name='images', permission='rwx'),
-            call(name='vms', permission='rwx'),
+            call(
+                name='volumes',
+                object_prefix_permissions={'class-read': ['rbd_children']},
+                permission='rwx'),
+            call(
+                name='images',
+                object_prefix_permissions={'class-read': ['rbd_children']},
+                permission='rwx'),
+            call(
+                name='vms',
+                object_prefix_permissions={'class-read': ['rbd_children']},
+                permission='rwx'),
         ])
 
     @patch('charmhelpers.core.hookenv.config')
